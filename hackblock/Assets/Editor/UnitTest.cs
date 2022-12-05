aca1e6c66bbfe7e0283633e5642eb9d6f4f7dc84
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2022-12-05 09:42:53 +0100

    pipeline

---
+++ hackblock/Assets/Editor/UnitTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+
+public class UnitTest : MonoBehaviour
+{
+    [Test]
+    public void test()
+    {
+        Assert.Pass();
+    }
+
+}
